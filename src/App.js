6105bb375814c8616c2dcb97f8f09c660b75e0a6
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2023-09-24 23:33:46 +0600

    Loader added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "../src/features/menu/Menu";
+import Cart from "../src/features/cart/Cart";
+import Order from "../src/features/order/Order";
+import CreatOrder from "../src/features/order/CreateOrder";
+import AppLayout from "./ui/AppLayout";
 
 const App = () => {
-  return <div>App</div>;
+  const router = createBrowserRouter([
+    {
+      element: <AppLayout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/menu", element: <Menu />, loader: menuLoader },
+        { path: "/cart", element: <Cart /> },
+        { path: "/order/new", element: <CreatOrder /> },
+        { path: "/order/:Orderid", element: <Order /> },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 };
 
 export default App;
