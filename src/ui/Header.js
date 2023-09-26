ad6a4b428500bf83e1c9b63010f366ac77cd4726
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2023-09-26 13:55:54 +0600

    order data loader and form action added

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -1,10 +1,12 @@
-import React from "react";
 import { Link } from "react-router-dom";
+
+import SrearchOrder from "../features/order/SearchOrder";
 
 const Header = () => {
   return (
     <header>
       <Link to="/">Fast React Pizza Co.</Link>
+      <SrearchOrder />
       <p>Ahmad</p>
     </header>
   );
